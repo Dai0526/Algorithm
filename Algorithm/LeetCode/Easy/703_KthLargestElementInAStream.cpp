a9d571ddf3375cc568bdf47b7562ff5a82b4c666
Tianhua Fang <tianhuaf0526@gmail.com>
2020-07-07 23:06:34 -0400

    Create 703_KthLargestElementInAStream.cpp

---
+++ Algorithm/LeetCode/Easy/703_KthLargestElementInAStream.cpp
@@ -0,0 +1,38 @@
+/*
+https://leetcode.com/problems/kth-largest-element-in-a-stream/submissions/
+keep a cap  = k's max heap
+
+*/
+
+#include <queue>
+#include <vector>
+using namespace std;
+
+class KthLargest {
+public:
+    KthLargest(int k, vector<int>& nums) {
+        cap = k;
+        for(int n : nums){
+            add(n);
+        }
+    }
+    
+    int add(int val) {
+        pq.push(val);
+        while(pq.size() > cap){
+            pq.pop();
+        }
+        
+        return pq.top();
+    }
+    
+private:
+    int cap;
+    priority_queue<int, vector<int>, greater<int>> pq;
+};
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest* obj = new KthLargest(k, nums);
+ * int param_1 = obj->add(val);
+ */
