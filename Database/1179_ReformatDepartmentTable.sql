e584798c648e75390c2852e25ef3d773b0810f14
Tianhua Fang <tianhuaf0526@gmail.com>
2020-08-02 19:09:32 -0400

    Create 1179_ReformatDepartmentTable.sql

---
+++ Database/1179_ReformatDepartmentTable.sql
@@ -0,0 +1,24 @@
+/*
+https://leetcode.com/problems/reformat-department-table/
+
+Use 
+1. IF FUNCTION
+2. CASE WHEN FUNCTION
+
+The Reason to use SUM is incase there are multiple records(rows) for the same months
+*/
+
+SELECT id, SUM( IF(month = 'Jan', revenue, NULL) ) AS Jan_Revenue
+         , SUM( IF(month = 'Feb', revenue, NULL) ) AS Feb_Revenue
+         , SUM( IF(month = 'Mar', revenue, NULL) ) AS Mar_Revenue
+         , SUM( IF(month = 'Apr', revenue, NULL) ) AS Apr_Revenue
+         , SUM( IF(month = 'May', revenue, NULL) ) AS May_Revenue
+         , SUM( IF(month = 'Jun', revenue, NULL) ) AS Jun_Revenue
+         , SUM( IF(month = 'Jul', revenue, NULL) ) AS Jul_Revenue
+         , SUM( IF(month = 'Aug', revenue, NULL) ) AS Aug_Revenue
+         , SUM( IF(month = 'Sep', revenue, NULL) ) AS Sep_Revenue
+         , SUM( IF(month = 'Oct', revenue, NULL) ) AS Oct_Revenue
+         , SUM( IF(month = 'Nov', revenue, NULL) ) AS Nov_Revenue
+         , SUM( IF(month = 'Dec', revenue, NULL) ) AS Dec_Revenue
+FROM Department
+GROUP BY id;
