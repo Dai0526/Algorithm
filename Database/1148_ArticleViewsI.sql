3465a6e5144c2664d911146cdb8afbc947ee6935
Tianhua Fang <tianhuaf0526@gmail.com>
2020-08-04 23:45:45 -0400

    Create 1148_ArticleViewsI.sql

---
+++ Database/1148_ArticleViewsI.sql
@@ -0,0 +1,34 @@
+/*
+https://leetcode.com/problems/article-views-i/
+
+Write an SQL query to find all the authors that viewed at least one of their own articles, sorted in ascending order by their id.
+
+The query result format is in the following example:
+
+Views table:
++------------+-----------+-----------+------------+
+| article_id | author_id | viewer_id | view_date  |
++------------+-----------+-----------+------------+
+| 1          | 3         | 5         | 2019-08-01 |
+| 1          | 3         | 6         | 2019-08-02 |
+| 2          | 7         | 7         | 2019-08-01 |
+| 2          | 7         | 6         | 2019-08-02 |
+| 4          | 7         | 1         | 2019-07-22 |
+| 3          | 4         | 4         | 2019-07-21 |
+| 3          | 4         | 4         | 2019-07-21 |
++------------+-----------+-----------+------------+
+
+Result table:
++------+
+| id   |
++------+
+| 4    |
+| 7    |
++------+
+
+*/
+SELECT author_id as id
+FROM Views
+WHERE author_id = viewer_id
+GROUP BY author_id
+ORDER BY id ASC;
