2eb527a6c1b7211a2c4ff83f184c82d54267c695
Tianhua Fang <tianhuaf0526@gmail.com>
2020-08-12 00:17:56 -0400

    Create 1084_SalesAnalysis3.sql

---
+++ Database/1084_SalesAnalysis3.sql
@@ -0,0 +1,43 @@
+/*
+
+Write an SQL query that reports the products that were only sold in spring 2019. That is, between 2019-01-01 and 2019-03-31 inclusive.
+
+The query result format is in the following example:
+
+Product table:
++------------+--------------+------------+
+| product_id | product_name | unit_price |
++------------+--------------+------------+
+| 1          | S8           | 1000       |
+| 2          | G4           | 800        |
+| 3          | iPhone       | 1400       |
++------------+--------------+------------+
+
+Sales table:
++-----------+------------+----------+------------+----------+-------+
+| seller_id | product_id | buyer_id | sale_date  | quantity | price |
++-----------+------------+----------+------------+----------+-------+
+| 1         | 1          | 1        | 2019-01-21 | 2        | 2000  |
+| 1         | 2          | 2        | 2019-02-17 | 1        | 800   |
+| 2         | 2          | 3        | 2019-06-02 | 1        | 800   |
+| 3         | 3          | 4        | 2019-05-13 | 2        | 2800  |
++-----------+------------+----------+------------+----------+-------+
+
+Result table:
++-------------+--------------+
+| product_id  | product_name |
++-------------+--------------+
+| 1           | S8           |
++-------------+--------------+
+The product with id 1 was only sold in spring 2019 while the other two were sold after.
+
+The idea to get the sale_date only in sprint
+1. max(sale_date) < '2019-03-31' AND min(sale_date) >= '2019-01-01' 
+2. sum(s.sale_date BETWEEN '2019-01-01' AND '2019-03-31')
+*/
+SELECT p.product_id, p.product_name 
+FROM Sales s JOIN Product p
+ON s.product_id = p.product_id
+GROUP BY p.product_id
+HAVING MAX(s.sale_date) < '2019-04-01' AND MIN(s.sale_date) >= '2019-01-01';
+
