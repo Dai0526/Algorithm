72d987e000ec2e08be92be1a98461a4f31f2a983
Tianhua Fang <tianhuaf0526@gmail.com>
2020-08-11 23:43:29 -0400

    Create 1076_ProjectEmployees2.sql

---
+++ Database/1076_ProjectEmployees2.sql
@@ -0,0 +1,45 @@
+/*
+https://leetcode.com/problems/project-employees-ii/
+
+Write an SQL query that reports all the projects that have the most employees.
+
+The query result format is in the following example:
+
+Project table:
++-------------+-------------+
+| project_id  | employee_id |
++-------------+-------------+
+| 1           | 1           |
+| 1           | 2           |
+| 1           | 3           |
+| 2           | 1           |
+| 2           | 4           |
++-------------+-------------+
+
+Employee table:
++-------------+--------+------------------+
+| employee_id | name   | experience_years |
++-------------+--------+------------------+
+| 1           | Khaled | 3                |
+| 2           | Ali    | 2                |
+| 3           | John   | 1                |
+| 4           | Doe    | 2                |
++-------------+--------+------------------+
+
+Result table:
++-------------+
+| project_id  |
++-------------+
+| 1           |
++-------------+
+The first project has 3 employees while the second one has 2.
+
+*/
+
+SELECT project_id
+FROM Project
+GROUP BY project_id
+HAVING COUNT(employee_id) = (SELECT COUNT(employee_id) AS cnt
+                              FROM Project
+                              GROUP BY project_id
+                              ORDER BY cnt DESC LIMIT 1)
