0675eceb7b57c92f2989e6454ac050aee590abe3
Tianhua Fang <tianhuaf0526@gmail.com>
2020-08-11 23:25:04 -0400

    Create 1075_ProjectEmployees1.sql

---
+++ Database/1075_ProjectEmployees1.sql
@@ -0,0 +1,41 @@
+/*
+https://leetcode.com/problems/project-employees-i/
+Write an SQL query that reports the average experience years of all the employees for each project, rounded to 2 digits.
+
+The query result format is in the following example:
+
+Project table:
++-------------+-------------+
+| project_id  | employee_id |
++-------------+-------------+
+| 1           | 1           |
+| 1           | 2           |
+| 1           | 3           |
+| 2           | 1           |
+| 2           | 4           |
++-------------+-------------+
+
+Employee table:
++-------------+--------+------------------+
+| employee_id | name   | experience_years |
++-------------+--------+------------------+
+| 1           | Khaled | 3                |
+| 2           | Ali    | 2                |
+| 3           | John   | 1                |
+| 4           | Doe    | 2                |
++-------------+--------+------------------+
+
+Result table:
++-------------+---------------+
+| project_id  | average_years |
++-------------+---------------+
+| 1           | 2.00          |
+| 2           | 2.50          |
++-------------+---------------+
+The average experience years for the first project is (3 + 2 + 1) / 3 = 2.00 and for the second project is (3 + 2) / 2 = 2.50
+
+*/
+SELECT p.project_id, ROUND(SUM(e.experience_years) / COUNT(p.project_id), 2) AS average_years
+FROM Project p JOIN Employee e
+ON p.employee_id = e.employee_id
+GROUP BY p.project_id
