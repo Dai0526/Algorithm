daedeab9efdaadf80141dca930a3f7f1aeb147e6
Tom Fang <tianhuaf0526@gmail.com>
2020-08-13 15:45:50 -0400

    Update 507_PerfectNumber.cpp
    Add a better solution

--- Algorithm/LeetCode/Easy/507_PerfectNumber.cpp
+++ Algorithm/LeetCode/Easy/507_PerfectNumber.cpp
@@ -1,13 +1,36 @@
 
 /*
+https://leetcode.com/problems/perfect-number/
 range from 1 to num/2
 
-
+There is an NlogN solution
 */
 
 class Solution {
 public:
+
+
     bool checkPerfectNumber(int num) {
+        if(num < 2){
+            return false;
+        }
+               
+        int sum = 1;
+            
+        for(int i = 2; i * i <= num; ++i){
+            if(num % i == 0){
+                sum += i;
+
+                if(i * i != num){
+                    sum += num/ i;
+                }
+            }
+        }
+        
+        return sum == num;
+    }
+
+    bool checkPerfectNumber_linear(int num) {
         if(num <= 0){
             return false;
         }
