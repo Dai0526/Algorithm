ce9949d4e0df200813582ece74e97cd6605f60dd
Tianhua Fang <tianhuaf0526@gmail.com>
2020-08-15 12:23:15 -0400

    add 1113

---
+++ Database/1113_ReportedPosts.sql
@@ -0,0 +1,38 @@
+/*
+Write an SQL query that reports the number of posts reported yesterday for each report reason. Assume today is 2019-07-05.
+
+The query result format is in the following example:
+
+Actions table:
++---------+---------+-------------+--------+--------+
+| user_id | post_id | action_date | action | extra  |
++---------+---------+-------------+--------+--------+
+| 1       | 1       | 2019-07-01  | view   | null   |
+| 1       | 1       | 2019-07-01  | like   | null   |
+| 1       | 1       | 2019-07-01  | share  | null   |
+| 2       | 4       | 2019-07-04  | view   | null   |
+| 2       | 4       | 2019-07-04  | report | spam   |
+| 3       | 4       | 2019-07-04  | view   | null   |
+| 3       | 4       | 2019-07-04  | report | spam   |
+| 4       | 3       | 2019-07-02  | view   | null   |
+| 4       | 3       | 2019-07-02  | report | spam   |
+| 5       | 2       | 2019-07-04  | view   | null   |
+| 5       | 2       | 2019-07-04  | report | racism |
+| 5       | 5       | 2019-07-04  | view   | null   |
+| 5       | 5       | 2019-07-04  | report | racism |
++---------+---------+-------------+--------+--------+
+
+Result table:
++---------------+--------------+
+| report_reason | report_count |
++---------------+--------------+
+| spam          | 1            |
+| racism        | 2            |
++---------------+--------------+ 
+Note that we only care about report reasons with non zero number of reports.
+
+*/
+SELECT extra as report_reason, COUNT(DISTINCT(post_id)) as report_count
+FROM Actions
+WHERE action_date = "2019-07-04" AND action = 'report'
+GROUP BY extra;
