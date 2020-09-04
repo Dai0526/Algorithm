953180597a1d1f9113b5ff2ef940319a46085721
Tianhua Fang <tianhuaf0526@gmail.com>
2020-09-03 20:50:09 -0700

    update 232

--- Algorithm/LeetCode/Easy/232_ImplementQueueUsingStack.cpp
+++ Algorithm/LeetCode/Easy/232_ImplementQueueUsingStack.cpp
@@ -18,53 +18,60 @@ class MyQueue {
 public:
     /** Initialize your data structure here. */
     MyQueue() {
-        size = 0;
+        
     }
     
     /** Push element x to the back of queue. */
     void push(int x) {
         stk1.push(x);
-        ++size;
     }
     
     /** Removes the element from in front of queue and returns that element. */
     int pop() {
-        int n = peek();
+        if(stk2.empty()){
+            while(!stk1.empty()){
+                stk2.push(stk1.top());
+                stk1.pop();
+            }
+        }
+        
+        int n = stk2.top();
         stk2.pop();
-        --size;
-        
         return n;
-        
     }
     
     /** Get the front element. */
     int peek() {
-       if(!stk2.empty()){
-           return stk2.top();
-       }
-        
-        while(!stk1.empty()){
-            int num = stk1.top();
-            stk2.push(num);
-            stk1.pop();
+        if(stk2.empty()){
+            while(!stk1.empty()){
+                stk2.push(stk1.top());
+                stk1.pop();
+            }
         }
         
-        return stk2.top();
+        int n = stk2.top();
+        return n;
     }
     
     /** Returns whether the queue is empty. */
     bool empty() {
-        return size == 0;
+        return stk1.empty() && stk2.empty();
     }
-
+    
 private:
-    stack<int> stk1; // real stack
-    stack<int> stk2; // function as queue
+    stack<int> stk1;
+    stack<int> stk2;
     
-    int size;
-    
-
 };
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
 
 /**
  * Your MyQueue object will be instantiated and called as such:
