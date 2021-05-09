cdc64a768b5b675fbfa2a1adbe159feb4ddf9ac0
Tianhua Fang <tianhuaf0526@gmail.com>
2021-05-08 21:21:21 -0400

    Create 32_longestValidParenthese.cpp

---
+++ Algorithm/LeetCode/Hard/32_longestValidParenthese.cpp
@@ -0,0 +1,36 @@
+
+/*
+
+Use stack
+
+*/
+
+#include <stack>
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        int length = 0;
+        stack<int> stk;
+        stk.push(-1);
+        
+        for(int i = 0; i < s.size(); ++i){
+            if(s[i] == '('){
+                stk.push(i);
+            }else{
+                stk.pop();
+                if(stk.empty()){
+                    stk.push(i); // push to stack as a index mark to update where the new beginning is
+                }else{
+                    length = max(length, i - stk.top());
+                }
+            }
+        }
+        
+        return length;
+                   
+    }
+};
