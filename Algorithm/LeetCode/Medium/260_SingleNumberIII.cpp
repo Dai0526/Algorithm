e500894a5a26514e55ad215fa138246c47a9d7fa
Tianhua Fang <tianhuaf0526@gmail.com>
2021-05-25 19:27:15 -0700

    Create 260_SingleNumberIII.cpp

---
+++ Algorithm/LeetCode/Medium/260_SingleNumberIII.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+        //create a bit mask which left over with x^y
+        long long mask = 0;
+        for(int n : nums){
+            mask = mask ^ n;
+        }
+        
+        int rmb = mask & (-mask);  // right most bit
+        
+        int a = 0, b = 0;
+        for(int n : nums){
+            if(n & rmb){
+                a = a ^ n;
+            }else{
+                b = b ^ n;
+            }
+        }
+        
+        return vector<int>{a, b};
+    }
+};
