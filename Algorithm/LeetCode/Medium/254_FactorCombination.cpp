08daf344fd7aeea12eb28a6310be1caede2bee07
Tianhua Fang <tianhuaf0526@gmail.com>
2021-05-25 19:20:57 -0700

    Update 254_FactorCombination.cpp

--- Algorithm/LeetCode/Medium/254_FactorCombination.cpp
+++ Algorithm/LeetCode/Medium/254_FactorCombination.cpp
@@ -29,3 +29,33 @@ public:
         comb.pop_back();    
     }
 };
+
+class Solution2 {
+public:
+    vector<vector<int>> getFactors(int n) {
+        vector<vector<int>> ans;
+        if(n == 1){
+            return ans;
+        }
+        
+        vector<int> comb;
+        dfs(ans, comb, n, 2);
+        
+        return ans;
+    }
+    
+    void dfs(vector<vector<int>>& ans, vector<int>& comb, int n, int factor){
+        int upperbound = sqrt(n);
+        for(int i = factor; i <= upperbound; ++i){
+            if(n % i != 0){
+                continue;
+            } 
+            
+            vector<int> prev(comb.begin(), comb.end());
+            prev.push_back(i);
+            dfs(ans, prev, n / i, i);
+            prev.push_back(n / i);
+            ans.push_back(prev);
+        }
+    }
+};
