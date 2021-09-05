89ace7ef9d90433cb3b393123f8ee60ede5f8ad2
Tianhua Fang <tianhuaf0526@gmail.com>
2021-09-05 16:49:36 -0700

    finish movie

--- Note/LearnJavaScript/6_Functional.md
+++ Note/LearnJavaScript/6_Functional.md
@@ -214,3 +214,20 @@ const averageRating = movies => {
 };
 ```
 
+At Last, the Movie.js is the eventual version:
+```Javascript
+const movieList = require("./MovieData.json");
+
+const titles = movies => movies.map(movie => movie.title);
+const byNolan = movie => movie.director === "Christopher Nolan";
+const filter = (movies, func) => movies.filter(func);
+const goodRating = movie => movie.imdbRating >= 7.5;
+const ratings = movies => movies.map(movie => movie.imdbRating);
+const average = array => array.reduce((sum, value) => sum + value, 0) / array.length;
+
+console.log(titles(movieList));
+const nolanMovieList = filter(movieList, byNolan);
+console.log(nolanMovieList.length);
+console.log(titles(filter(movieList, goodRating)));
+console.log(average(ratings(nolanMovieList)));
+```
--- Note/LearnJavaScript/Example/Functional/Movie.js
+++ Note/LearnJavaScript/Example/Functional/Movie.js
@@ -1,65 +1,14 @@
 const movieList = require("./MovieData.json");
 
-// Get movie titles
-const titles = movies => {
-    /* Previous code
-    const titles = [];
-    for (const movie of movies) {
-      titles.push(movie.title);
-    }
-    return titles;
-    */
-  
-    // Return a new array containing only movie titles
-    return movies.map(movie => movie.title);
-  };
+const titles = movies => movies.map(movie => movie.title);
+const byNolan = movie => movie.director === "Christopher Nolan";
+const filter = (movies, func) => movies.filter(func);
+const goodRating = movie => movie.imdbRating >= 7.5;
+const ratings = movies => movies.map(movie => movie.imdbRating);
+const average = array => array.reduce((sum, value) => sum + value, 0) / array.length;
 
-  // Get movies by Christopher Nolan
-const nolanMovies = movies => {
-    /* Previous code
-    const nolanMovies = [];
-    for (const movie of movies) {
-      if (movie.director === "Christopher Nolan") {
-        nolanMovies.push(movie);
-      }
-    }
-    return nolanMovies;
-    */
-  
-    // Return a new array containing only movies by Christopher Nolan
-    return movies.filter(movie => movie.director === "Christopher Nolan");
-  };
-
-  // Get titles of movies with an IMDB rating greater or equal to 7.5
-const bestTitles = movies => {
-    /* Previous code
-    const bestTitles = [];
-    for (const movie of movies) {
-      if (movie.imdbRating >= 7.5) {
-        bestTitles.push(movie.title);
-      }
-    }
-    return bestTitles;
-    */
-  
-    // Filter movies by IMDB rating, then creates a movie titles array
-    return movies.filter(movie => movie.imdbRating >= 7.5).map(movie => movie.title);
-  };
-
-  // Compute average rating of a movie list
-const averageRating = movies => {
-    /* Previous code
-    let ratingSum = 0;
-    for (const movie of movies) {
-      ratingSum += movie.imdbRating;
-    }
-    return ratingSum / movies.length;
-    */
-  
-    // Compute the sum of all movie IMDB ratings
-    const ratingSum = movies.reduce((acc, movie) => acc + movie.imdbRating, 0);
-    //const ratingSum = movies.map(movie => movie.imdbRating).reduce((acc, value) => acc + value, 0);
-
-    return ratingSum / movies.length;
-  };
-
+console.log(titles(movieList));
+const nolanMovieList = filter(movieList, byNolan);
+console.log(nolanMovieList.length);
+console.log(titles(filter(movieList, goodRating)));
+console.log(average(ratings(nolanMovieList)));
