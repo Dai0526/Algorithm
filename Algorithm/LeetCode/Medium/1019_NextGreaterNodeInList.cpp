541ca580b39233c8e9760273797488dda53ed19d
Tianhua Fang <tianhuaf0526@gmail.com>
2021-09-13 16:01:08 -0700

    1019 add comment

--- Algorithm/LeetCode/Medium/1019_NextGreaterNodeInList.cpp
+++ Algorithm/LeetCode/Medium/1019_NextGreaterNodeInList.cpp
@@ -8,6 +8,22 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+/*
+
+1. use a stack to record the index of previous unfound value
+2. use vector to save ans
+
+iterate list
+
+If stack not empty(there is value unfound） && that value is less then current:
+    means it is the next larger value of it, then update ans with this value at idx
+
+push current ans size as the index to the stack.
+push list node value to vector
+
+*/
+
 class Solution {
 public:
     vector<int> nextLargerNodes(ListNode* head) {
