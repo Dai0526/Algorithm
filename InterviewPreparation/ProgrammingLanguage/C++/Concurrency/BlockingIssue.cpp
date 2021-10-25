9e290e3082786f472b2e39993f56de64386a7511
Tianhua Fang <tianhuaf0526@gmail.com>
2021-10-24 22:47:34 -0700

    add multithread issue code

---
+++ InterviewPreparation/ProgrammingLanguage/C++/Concurrency/BlockingIssue.cpp
@@ -0,0 +1,51 @@
+﻿/*
+initialization sequence of threads will cause this issue
+
+Because t2 init before t1
+that t2 notify before t1 is wait
+and t1 is locked because it misses the t2 notify
+*/
+
+#include <iostream>
+#include <condition_variable>
+#include <mutex>
+#include <thread>
+
+std::mutex mutex_;
+std::condition_variable condVar;
+
+bool dataReady;
+
+
+void waitingForWork(){
+
+    std::cout << "Worker: Waiting for work." << std::endl;
+
+    std::unique_lock<std::mutex> lck(mutex_);
+    condVar.wait(lck);                          
+    // do the work
+    std::cout << "Work done." << std::endl;
+
+}
+
+void setDataReady(){
+
+    std::cout << "Sender: Data is ready."  << std::endl;
+    condVar.notify_one();                        
+
+}
+
+int main(){
+
+  std::cout << std::endl;
+
+  std::thread t1(setDataReady); // t2 will miss t1's notify for cv
+  std::thread t2(waitingForWork); 
+   
+
+  t1.join();
+  t2.join();
+
+  std::cout << std::endl;
+  
+}
---
+++ InterviewPreparation/ProgrammingLanguage/C++/Concurrency/DataRaces.cpp
@@ -0,0 +1,37 @@
+﻿// addMoney.cpp
+
+#include <functional>
+#include <iostream>
+#include <thread>
+#include <vector>
+
+struct Account{
+  int balance{100};                             
+};
+
+void addMoney(Account& to, int amount){
+  to.balance += amount;                         
+}
+
+void Add5000(){
+      
+
+    Account account;
+    std::vector<std::thread> vecThreads(100);
+  
+                                                 
+    for (auto& thr: vecThreads) thr = std::thread(addMoney, std::ref(account), 50);
+  
+    for (auto& thr: vecThreads) thr.join();
+  
+                                                 
+    std::cout << "accou nt.balance: " << account.balance << std::endl;
+}
+
+int main(){
+    for(int i = 0; i < 10; ++i){
+        Add5000();
+    }
+
+
+}
