f5c319d2afc487ee019c8d6f76a14125d7b812be
Tianhua Fang <tianhuaf0526@gmail.com>
2021-10-31 12:06:03 -0700

    Create 14_LongestCommonPrefix.cpp

---
+++ Algorithm/OnlineAssessment/LeetCode/Easy/14_LongestCommonPrefix.cpp
@@ -0,0 +1,32 @@
+﻿/*
+Brute Force Solution
+https://leetcode.com/problems/longest-common-prefix/
+*/
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if(strs.size() == 0){
+            return "";
+        }
+        
+        string prefix = "";
+        
+        for(int i = 0; i < strs[0].length(); ++i){
+            for(int j = 0; j < strs.size(); ++j){
+                
+                if(i >= strs[j].length()){
+                    return prefix;
+                }
+                
+                if(strs[j][i] != strs[0][i]){
+                    return prefix;
+                }
+            }
+            
+            prefix += strs[0][i];
+        }
+        
+        return prefix;
+    }
+};
