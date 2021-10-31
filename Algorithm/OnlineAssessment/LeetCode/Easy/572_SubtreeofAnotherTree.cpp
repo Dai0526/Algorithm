040841405f91a6cd677c96caa5fa70cbdb07f47b
Tianhua Fang <tianhuaf0526@gmail.com>
2021-10-31 15:58:55 -0700

    Create 572_SubtreeofAnotherTree.cpp

---
+++ Algorithm/OnlineAssessment/LeetCode/Easy/572_SubtreeofAnotherTree.cpp
@@ -0,0 +1,46 @@
+﻿/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+        
+        if(root == nullptr){
+            return subRoot == nullptr;
+        }
+        
+        if(root->val == subRoot->val){
+            if(compare(root, subRoot)){
+                return true;
+            }
+        }
+        
+        return isSubtree(root->left, subRoot) || isSubtree(root->right, subRoot);
+        
+    }
+    
+    bool compare(TreeNode* root, TreeNode* subRoot){
+        if(root == nullptr){
+            return subRoot == nullptr;
+        }
+        
+        if(subRoot == nullptr){
+            return false;
+        }
+        
+        if(root->val != subRoot->val){
+            return false;
+        }
+        
+        
+        return compare(root->left, subRoot->left) && compare(root->right, subRoot->right);
+    }
+};
