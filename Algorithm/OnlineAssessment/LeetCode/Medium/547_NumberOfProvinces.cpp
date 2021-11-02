a67a3cffc18989835a46fea369808cb8b772a9f8
Tianhua Fang <tianhuaf0526@gmail.com>
2021-11-01 22:28:20 -0700

    Create 547_NumberOfProvinces.cpp

---
+++ Algorithm/OnlineAssessment/LeetCode/Medium/547_NumberOfProvinces.cpp
@@ -0,0 +1,30 @@
+﻿/*
+DFS
+
+*/
+
+class Solution {
+public:
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        unordered_set<int> visited;
+        int count = 0;
+        
+        for(int i = 0; i < isConnected.size(); ++i){
+            if(visited.find(i) == visited.end()){
+                dfs(visited, isConnected, i);
+                ++count;
+            }
+        }
+        
+        return count;
+    }
+    
+    void dfs(unordered_set<int>& visited, vector<vector<int>>& isConnected, int curr){
+        for(int i = 0; i < isConnected.size(); ++i){
+            if(isConnected[curr][i] == 1 && visited.find(i) == visited.end()){
+                visited.insert(i);
+                dfs(visited, isConnected, i);
+            }
+        }
+    }
+};
