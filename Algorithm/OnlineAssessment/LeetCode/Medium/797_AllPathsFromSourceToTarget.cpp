065f459bd4972ef5ccac1ee0d6b2605922666163
Tianhua Fang <tianhuaf0526@gmail.com>
2021-11-01 22:14:36 -0700

    Create 797_AllPathsFromSourceToTarget.cpp

---
+++ Algorithm/OnlineAssessment/LeetCode/Medium/797_AllPathsFromSourceToTarget.cpp
@@ -0,0 +1,36 @@
+﻿/*
+DFS with backtracking
+https://leetcode.com/problems/all-paths-from-source-to-target/
+*/
+
+class Solution {
+public:
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+        
+        int n = graph.size();
+        
+        vector<vector<int>> ans;
+        vector<int> path;
+        
+        path.push_back(0);
+        dfs(graph, n - 1, 0, path, ans);
+        
+        return ans;
+        
+    }
+    
+    void dfs(vector<vector<int>>& graph, const int dest, int curr, vector<int>& path, vector<vector<int>>& ans){
+        if(curr == dest){
+            ans.push_back(path);
+            return;
+        }
+        
+        vector<int> nexts = graph[curr];
+        
+        for(int n : nexts){
+            path.push_back(n);
+            dfs(graph, dest, n, path, ans);
+            path.pop_back();
+        }
+    }
+};
