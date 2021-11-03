66689af6cee6fa3d1d816c8df175f620c1c5e369
Tianhua Fang <tianhuaf0526@gmail.com>
2021-11-02 22:48:59 -0700

    Create 55_JumpGame.cpp

---
+++ Algorithm/OnlineAssessment/LeetCode/Medium/55_JumpGame.cpp
@@ -0,0 +1,34 @@
+﻿class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+           
+        if(n == 1){
+            return true;
+        }
+        
+        vector<int> lut(n, -1);
+        lut[0] = 1;
+        
+        for(int i = 0; i < n - 1; ++i){
+            if(lut[i] == -1){
+                return false;
+            }
+            
+            for(int j = 1; j <= nums[i]; ++j){
+                
+                int idx = i + j;
+                
+                if(idx == n - 1){
+                    return true;
+                }
+                
+                lut[i + j] = 1;
+            }
+            
+        }
+        
+        return false;
+        
+    }
+};
