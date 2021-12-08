6b698ad9ff0b4133460664483d9d5694f5739497
Tianhua Fang <tianhuaf0526@gmail.com>
2021-12-07 21:42:35 -0800

    Create 299_BullsAndCows.cpp

---
+++ Algorithm/OnlineAssessment/LeetCode/Medium/299_BullsAndCows.cpp
@@ -0,0 +1,35 @@
+﻿/*
+https://leetcode.com/problems/bulls-and-cows/
+*/
+
+class Solution {
+public:
+    string getHint(string secret, string guess) {
+        unordered_map<char, int> secMap;
+        unordered_map<char, int> gesMap;
+        
+        // get bulls;
+        int nBulls = 0;
+        for(int i = 0; i < secret.length(); ++i){
+            if(secret[i] == guess[i]){
+                ++nBulls;
+            }else{
+                // only save un-bull candidates
+                ++secMap[secret[i]];
+                ++gesMap[guess[i]];
+            }
+
+        }
+        
+        
+        int nCows = 0;
+        for(const pair<char, int>& pr : secMap){
+            nCows += min(pr.second, gesMap[pr.first]);
+        }
+        
+        stringstream ss;
+        ss << nBulls << "A" << nCows << "B";
+        
+        return ss.str();
+    }
+};
