68094f21267bd1f4600aec79a7420141e7e4dd74
Tianhua Fang <tianhuaf0526@gmail.com>
2021-12-07 21:28:34 -0800

    Create 370_RangeAddition.cpp

---
+++ Algorithm/OnlineAssessment/LeetCode/Medium/370_RangeAddition.cpp
@@ -0,0 +1,41 @@
+﻿/*
+https://leetcode.com/problems/range-addition/
+cache all modifications, then do a partial sum
+
+*/
+
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> getModifiedArray(int length, vector<vector<int>>& updates) {
+        vector<int> ans(length, 0);
+        
+        int nUpdates = updates.size();
+        
+        for(int i = 0; i < nUpdates; ++i){
+            vector<int> temp = updates[i];
+            
+            int start = temp[0];
+            int end = temp[1];
+            int inc = temp[2];
+            
+            ans[start] += inc;
+            
+            if(end + 1 < length){
+                ans[end + 1] -= inc;
+            }
+            
+        }
+        
+        
+        // calculate partial sum
+        for(int i = 1; i < length; ++i){
+            ans[i] += ans[i - 1];
+        }
+        
+        return ans;
+        
+    }
+};
