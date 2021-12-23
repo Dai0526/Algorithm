c3abe76e9e3600d7361dd98790daa1e2be1886cb
Tianhua Fang <tianhuaf0526@gmail.com>
2021-12-22 21:19:52 -0800

    add 383 and 389

---
+++ Algorithm/OnlineAssessment/LeetCode/Easy/383_RansomNote.cpp
@@ -0,0 +1,33 @@
+﻿/*
+construct map 
+remove count 
+
+*/
+
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        unordered_map<char, int> magaMap;
+        
+        for(char c : magazine){
+            ++magaMap[c];
+        }
+        
+        for(char c : ransomNote){
+            if(magaMap.find(c) == magaMap.end()){
+                return false;
+            }
+            
+            if(magaMap[c] == 0){
+                return false;
+            }
+            
+            --magaMap[c];
+            
+        }
+        
+        
+        return true;
+        
+    }
+};
---
+++ Algorithm/OnlineAssessment/LeetCode/Easy/389_FindTheDifference.cpp
@@ -0,0 +1,21 @@
+﻿class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        unordered_map<char, int> sMap;
+        char target = 0;
+        
+        for(char c : s){
+            ++sMap[c];
+        }
+        
+        for(char c : t){
+            if(sMap.find(c) == sMap.end() || sMap[c] == 0){
+                target = c;
+                break;
+            }
+            --sMap[c];
+        }
+        
+        return target;
+    }
+};
