732ec233cfd48c7001001f5bf6fb79fce7bf1a5a
Tianhua Fang <tianhuaf0526@gmail.com>
2022-02-23 22:36:15 -0800

    Create CoinExchanges.cpp

---
+++ Algorithm/Work/CoinExchanges.cpp
@@ -0,0 +1,34 @@
+﻿#include <vector>
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int coinExchange(const vector<int>& coins, int val){
+    int n = coins.size();
+    vector<int> lut(val + 1, 0);
+
+    for(int i = 1; i <= val; ++i){
+        int minCount = numeric_limits<int>::max();
+        for(int j = 0; j < n; ++j){
+            if(i >= coins[j]){
+                minCount = min(minCount, 1 + lut[i - coins[j]]);
+                cout << "i = " << i << ", j = " << j << ", min = " << minCount << endl;
+                lut[i] = minCount;
+            }
+        }   
+    }
+
+    return lut[val];
+}
+
+
+int main(int argc, char *argv[]){
+    
+    int value = 24;
+    vector<int> coins { 1, 5, 8, 15, 20};
+
+    cout << "ans is " << coinExchange(coins, value) << endl;
+
+    return 0;
+}
