f5723ad9ea433da8f05f92d958fe57f88559b3c2
Tianhua Fang <tianhuaf0526@gmail.com>
2022-03-20 16:20:38 -0700

    Create 1047_RemoveAllAdjacentDuplicatesinString.cpp

---
+++ Algorithm/OnlineAssessment/LeetCode/Easy/1047_RemoveAllAdjacentDuplicatesinString.cpp
@@ -0,0 +1,31 @@
+﻿/*
+https://leetcode.com/problems/remove-all-adjacent-duplicates-in-string/
+*/
+
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        stack<char> stk;
+        stk.push(s[0]);
+        
+        for(int i = 1; i < s.size(); ++i){
+            if(!stk.empty() && stk.top() == s[i]){
+                stk.pop();
+            }else{
+                stk.push(s[i]);
+            }
+        }
+        
+        stringstream ss;
+        while(!stk.empty()){
+            ss << stk.top();
+            stk.pop();
+        }
+        
+        string ans = ss.str();
+        reverse(ans. begin(), ans.end());
+        
+        return ans;
+        
+    }
+};
