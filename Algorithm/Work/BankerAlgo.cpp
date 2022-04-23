d2685597c86d7970f42da20a267e6d12e3f1e50d
Tianhua Fang <tianhuaf0526@gmail.com>
2022-04-23 09:00:12 -0700

    start banker's algo

---
+++ Algorithm/Work/BankerAlgo.cpp
@@ -0,0 +1,23 @@
+﻿#include <exception>
+#include <iostream>
+
+using namespace std;
+
+void Bankers(){
+
+}
+
+int main(){
+    
+    try{
+        Bankers();
+    }catch(exception & ex){
+        cerr << ex.what() << endl;
+        return 1;
+    }catch(...){
+        cerr << "Unknow exception" << endl;
+        return 1;
+    }
+
+    return 0;
+}
