0f7c403af3ae9b2c5d526da679a26f4a3b9922d4
Tianhua Fang <tianhuaf0526@gmail.com>
2022-09-13 23:20:56 -0700

    Create 405_ConvertNumberToHex.cpp

---
+++ Algorithm/OnlineAssessment/LeetCode/Easy/405_ConvertNumberToHex.cpp
@@ -0,0 +1,31 @@
+﻿#include <sstream>
+#include <algorithm>
+#include <iostream>
+
+class Solution {
+public:
+    string toHex(int num) {
+        if(num == 0){
+            return "0";
+        }
+        
+        const int DIV = 16;
+        const string HEX_MAPPING_STR = "0123456789abcdef";
+        
+        stringstream ss;
+        // convert to 2's complement rep for negative number
+        unsigned int temp = num; 
+        
+        while(temp > 0){
+            int nIdx = temp % DIV;
+            char cHex = HEX_MAPPING_STR[nIdx];
+            ss << cHex;
+            temp = temp / DIV;
+        }
+        
+        string ans = ss.str();
+        reverse(ans.begin(), ans.end()); // in reverse order
+        
+        return ans;
+    }
+};
