de303c18fd0cb7205aabba1df5ca41054450830c
Tianhua Fang <tianhuaf0526@gmail.com>
2022-09-15 21:01:29 -0700

    Create 476_NumberComplement.cpp

---
+++ Algorithm/OnlineAssessment/LeetCode/Easy/476_NumberComplement.cpp
@@ -0,0 +1,26 @@
+﻿/*
+https://leetcode.com/problems/number-complement/solution/
+
+Do bit by bit
+1. mask = 1
+2. xor with number to flip bit
+3. shift
+
+*/
+
+class Solution {
+public:
+    int findComplement(int num) {
+        int target = num;
+        int bit = 1;
+        
+        while(target != 0){
+            
+            num = num ^ bit; // flip bit
+            bit = bit << 1;  // shift mask 
+            target = target >> 1; // shift target
+        }
+        
+        return num;
+    }
+};
