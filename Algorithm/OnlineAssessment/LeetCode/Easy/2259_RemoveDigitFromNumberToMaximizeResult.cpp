6e3f753e4f7f50f42c02b0fb0f80ab72e058f619
Tianhua Fang <tianhuaf0526@gmail.com>
2022-10-10 22:39:21 -0700

    add 867 and 2259

---
+++ Algorithm/OnlineAssessment/LeetCode/Easy/2259_RemoveDigitFromNumberToMaximizeResult.cpp
@@ -0,0 +1,36 @@
+﻿/*
+https://leetcode.com/problems/remove-digit-from-number-to-maximize-result/
+
+if remove a digit, and next digit is greater than the one removed, which means
+remove it increase the value.
+
+We start from left, thus, the earlier it met the above condition, the larger it will be.
+
+Find that index, and do a string concatenation. 
+
+*/
+
+#include <sstream>
+
+using namespace std;
+
+class Solution {
+public:
+    string removeDigit(string number, char digit) {
+
+        int idx, len = number.size();
+        for (int i = 0; i < len; i++) {
+            if (number[i] != digit) {
+                continue;   
+            }
+            //12121
+            idx = i;
+            if (number[i+1] > number[i]) {
+                break;
+            }
+
+        }
+        return number.substr(0, idx) + number.substr(idx + 1);
+    }
+};
+
---
+++ Algorithm/OnlineAssessment/LeetCode/Easy/867_TransposeMatrix.cpp
@@ -0,0 +1,28 @@
+﻿/*
+https://leetcode.com/problems/transpose-matrix/
+
+Idea:
+* Direct copy
+* in place will be another story
+
+
+*/
+
+class Solution {
+public:
+    vector<vector<int>> transpose(vector<vector<int>>& matrix) {
+        int r = matrix.size();
+        int c = matrix[0].size();
+        
+        vector<vector<int>> tp(c, vector<int>(r));
+        
+        for(int i = 0; i < r; ++i){
+            for(int j = 0; j < c; ++j){
+                tp[j][i] = matrix[i][j];
+            }
+        }
+        
+        
+        return tp;
+    }
+};
