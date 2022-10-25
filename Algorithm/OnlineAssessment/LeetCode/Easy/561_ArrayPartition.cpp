cf6e962156028db7e33f5d87c558a3c609fc0872
Tianhua Fang <tianhuaf0526@gmail.com>
2022-10-24 22:28:39 -0700

    Create 561_ArrayPartition.cpp

---
+++ Algorithm/OnlineAssessment/LeetCode/Easy/561_ArrayPartition.cpp
@@ -0,0 +1,28 @@
+﻿/*
+https://leetcode.com/problems/array-partition/
+
+Idea:
+sort, then add every other numbers
+
+*/
+
+
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int arrayPairSum(vector<int>& nums) {
+        
+        std::sort(nums.begin(), nums.end());
+        int maxSum = 0;
+        
+        for(int i = 0; i < nums.size(); i+=2){
+            maxSum += nums[i];
+        }
+        
+        return maxSum;
+    }
+};
