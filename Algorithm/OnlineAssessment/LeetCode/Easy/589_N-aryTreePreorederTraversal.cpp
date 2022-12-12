da052467b8628702a2f84ba685b49fed931f5a38
Tianhua Fang <tianhuaf0526@gmail.com>
2022-12-11 22:02:11 -0800

    Create 589_N-aryTreePreorederTraversal.cpp

---
+++ Algorithm/OnlineAssessment/LeetCode/Easy/589_N-aryTreePreorederTraversal.cpp
@@ -0,0 +1,59 @@
+﻿/*
+https://leetcode.com/problems/n-ary-tree-preorder-traversal/submissions/858470500/
+*/
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    vector<int> preorder(Node* root) {
+        
+        if(root == nullptr){
+            return vector<int>();
+        }
+
+        vector<int> ans;
+        stack<Node*> myStack;
+
+        myStack.push(root);
+
+        while(myStack.size() > 0){
+            
+            Node* curr = myStack.top();
+            myStack.pop();
+            ans.push_back(curr->val);
+
+            if(curr->children.size() == 0){
+                continue;
+            }
+
+            reverse(curr->children.begin(), curr->children.end());
+
+            for(Node* pNode : curr->children){
+                myStack.push(pNode);
+            }
+
+        }
+
+        return ans;
+
+    }
+};
